17be65eb16409839a30ee253f43d47fcdab2d261
ASC2227548 <2227548@brunel.ac.uk>
2024-04-17 20:01:41 +0100

    ground Scrolling (fix not coming into screen in time)

--- Object-Orientated Game - 2227548.py
+++ Object-Orientated Game - 2227548.py
@@ -3,23 +3,56 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 864
-screen_height = 936
+clock = pygame.time.Clock()
+fps = 60
+
+#Screen size when you play
+screen_width = 640
+screen_height = 640
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Assessment 3: Jumpy Ship ')
 
-#Images into game
-bg = pygame.image.load('assets/')
+#Load images into game
+bg = pygame.image.load('assets/bg.png')
+ground = pygame.image.load('assets/ground.jpg')
+
+#game variables
+ground_move = 0
+ground_speed = 4
+
+bg0x = 545
+bg1x = -screen_width
+def rolling_ground():
+    global bg0x, bg1x, screen_width
+    screen.blit(ground, (bg0x,545))
+    screen.blit(ground, (bg1x,545))
+    bg0x += 1
+    bg1x += 1
+    if bg0x > screen_width:
+        bg0x = -screen_width
+    if bg1x > screen_width:
+        bg1x = -screen_width
 
 
-
-
+#Game loop
 run = True
 while run:
+
+    clock.tick(fps)
+
+    #Draw the backgound
+    screen.blit(bg, (0,0))
+
+    #draw and move the ground
+    rolling_ground()
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
